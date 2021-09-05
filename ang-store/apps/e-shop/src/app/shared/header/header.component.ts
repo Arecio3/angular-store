4e27def8f6037885c13f8bc7cbc49e6c49847f98
Arecio Canton <cuba289@gmail.com>
2021-09-04 22:19:04 -0400

    styles file structure

--- ang-store/apps/e-shop/src/app/shared/header/header.component.ts
+++ ang-store/apps/e-shop/src/app/shared/header/header.component.ts
@@ -2,8 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'ang-store-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  templateUrl: './header.component.html'
 })
 export class HeaderComponent implements OnInit {
 
